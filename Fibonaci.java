52d45bcfd4a33247d11d2e6e7e7c0ca1f92bbb1a
dina4133 <dinajunirestina@gmail.com>
2016-03-29 15:19:59 +0700

    Added files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jur_1;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Fibonaci {
+    private ArrayList<Integer> al = new ArrayList<>();
+    
+    public Fibonaci() {
+    }
+    
+    public void performFibonaci(int i){
+        int prevInt = 0;
+        int nextInt = 1;
+        for (int j = 0; j < i; j++) {
+            if ( j == 0 || j == 1){
+                System.out.println("1");
+                al.add(1);
+                continue;
+            }
+            al.add(al.get(j-1)+al.get(j-2));
+            System.out.println("" + al.get(j));
+        }
+        
+    }
+}
---
+++ Jur_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jur_1;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Jur_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fibonaci1 fibonaci = new Fibonaci1();
+        fibonaci.performFibonaci1(4);
+    }
+    
+}
